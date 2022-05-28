b29528430e7ab32243d4030a9c6522b21dbe690f
Rustam Tazyaev <rustytazyaev@gmail.com>
2022-05-28 13:45:03 +0400

    created modules and services for auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import {AdminModule} from "./admin/admin.module";
+import {UserModule} from "./user/user.module";
+import {UserWorkModule} from "./user-work/user-work.module";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdminModule,
+    UserModule,
+    UserWorkModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
